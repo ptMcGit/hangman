6df28b119e17a7fb7121b2a789788a268a6ec5c6
gazelle <gazelle@aardvarks-MacBook-Pro.local>
2016-05-17 16:51:43 -0400

    added 'get a hint' feature

--- hangman.rb
+++ hangman.rb
@@ -3,14 +3,15 @@
 require "pry"
 # word list
 
-max_word_length = 3
+min_word_length = 5
+max_word_length = 10
 
 word_list = []
 
 
 f = File.open("/usr/share/dict/words", "r")
 f.each do |line|
-  if line.length <= max_word_length
+  if line.length <= max_word_length && line.length >= min_word_length
     word_list.push line.chomp
   end
   #print "line: "
@@ -22,39 +23,73 @@ guesses_allowed = 6
 word = word_list[rand(word_list.count)]
 word_length = word.length
 
-letters = word.downcase.split("")
+$letters = word.downcase.split("")
 
-correct_g = []
+$correct_g = []
 0.upto word_length - 1 do |x|
   #print x
-  correct_g[x] = "_"
+  $correct_g[x] = "_"
 end
 
 #binding.pry
+$allowed_chars = []
 
-allowed_chars = []
 
 97.upto 122 do |x|
-  allowed_chars.push x.chr
+  $allowed_chars.push x.chr
 end
-
 
 game_end = false
 win = false
+
+$hints = 3
+
+def get_hint
+  #allowed_hints = $allowed_chars
+  #allowed_hints.delete $correct_g.uniq
+  
+
+  if $hints < 1
+    return nil
+  else
+    #$allowed_chars.each do |x|
+    0.upto $letters.count - 1 do |x|
+      
+      if $letters[x] != $correct_g[x]
+        $hints -= 1
+        binding.pry
+        return $letters[x]
+      end
+      
+    end
+  end
+end
+
+
 until game_end
   
   #binding.pry
-  correct_g.each do |x|
+  $correct_g.each do |x|
     print x
     print " "
   end
   puts
-  puts "You have #{guesses_allowed} guesses left."
-  print "What is your guess? "
+  puts "You have #{guesses_allowed} guesses left (#{$hints} hints left)."
+  print "What is your guess? (Enter '?' for a hint) "
 
   guess = gets.chomp.to_s.downcase
 
-  unless allowed_chars.include? guess
+  if guess == "?"
+    guess = get_hint
+  end
+  if guess == nil
+    puts "You have no more hints!"
+    next
+  end
+
+
+  
+  unless $allowed_chars.include? guess
     puts "That is not a valid character."
     next
   end
@@ -65,17 +100,17 @@ until game_end
     win = true
   end
   
-  if letters.include? guess
+  if $letters.include? guess
     0.upto word_length - 1 do |x|
-      if guess == letters[x]
-        correct_g[x] = guess
+      if guess == $letters[x]
+        $correct_g[x] = guess
       end
     end
   else
     guesses_allowed -= 1
   end
 
-  if correct_g == letters
+  if $correct_g == $letters
     #binding.pry
     game_end = true
     win = true
@@ -98,5 +133,6 @@ if win
 else
   puts "You lose!"
 end
+
 
 #print "enter a word: "
