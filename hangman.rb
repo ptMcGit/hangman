4ea380c9ce60d58d09c55f4709890a1ef79d66f4
Zach H <zdhadgraft@gtcc.edu>
2016-05-18 16:32:24 -0400

    Refactored
    - Eliminated use of global variables
    - Added functions
    - Improved presentation

--- hangman.rb
+++ hangman.rb
@@ -1,62 +1,69 @@
 #  Hangman
 
-min_word_length = 5
-max_word_length = 10
-guesses_allowed = 6
+require "pry"
+valid_words = []
+guess_list = []
+allowed_chars = ("a".."z").to_a
 
-word_list = []
-$guess_list = []
+def load_words (file, min, max)
 
-f = File.open("/usr/share/dict/words", "r")
+  # load in words from file that are of lengths between min and max
 
-f.each do |line|
-  if line.length <= max_word_length && line.length >= min_word_length
-    word_list.push line.chomp
+  valid_words = []
+  File.open(file, "r") do |f_handle|
+    f_handle.each_line do |line|
+      line = line.chomp
+      if line.length <= max && line.length >= min
+        valid_words.push line
+      end
+    end
+  end
+  return valid_words
+end
+
+def get_letter_freq_ascending(lines)
+
+  # returns a hash with frequency of each letter in ascending order
+
+  chars = ("a".."z")
+  h = Hash[chars.map {|x| [x, 0]}]
+
+  lines.each do |item|
+    item.split("").each do |letter|
+      letter.downcase!
+      chars.include? letter && h[letter] += 1
+    end
+  end
+
+  h = h.sort_by { |a, b| b }
+  h = h.map { |a,b| a }
+
+  return h
+end
+
+def play_again?
+  while true
+    print "Would you like to play again? (y/n) "
+    response = gets.chomp
+    if response == "y"
+      return true
+    elsif response == "n"
+      return false
+    else
+      next
+    end
   end
 end
 
-word = word_list[rand(word_list.count)]
-word_length = word.length
+def get_hint(sorted_letters, correct_guesses, word, guess_list)
 
-$letters = word.downcase.split("")
+  # submit an hint that will reduce the remaining letters to guess by one
 
-$correct_g = []
-
-0.upto word_length - 1 do |x|
-  $correct_g[x] = "_"
-end
-
-$allowed_chars = []
-$freq_list = {}
-
-97.upto 122 do |x|
-  $allowed_chars.push x.chr
-  $freq_list[x.chr] = 0
-end
-
-word_list.each do |item|
-  item.split("").each do |letter|
-    $freq_list[letter.downcase] += 1
-  end
-end
-
-$freq_list = $freq_list.sort_by { |a, b| b }
-$freq_list = $freq_list.map { |a,b| a }
-
-game_end = false
-win = false
-
-$hints = 3
-
-def get_hint
-  allowed_hints = $freq_list
-  allowed_hints = allowed_hints - $correct_g.uniq
-  allowed_hints = allowed_hints - $guess_list.uniq
-
+  allowed_hints = sorted_letters - correct_guesses.uniq - guess_list.uniq
   index = 0
 
   until index == allowed_hints.length - 1
-    unless $letters.include? allowed_hints[index]
+    unless word.include? allowed_hints[index]
       allowed_hints.delete_at(index)
       next
     else
@@ -64,86 +71,134 @@ def get_hint
     end
   end
 
-  if $hints < 1
-    return nil
-  else
-    $hints -= 1
-    return allowed_hints[0]
-  end
-
+  return allowed_hints[0]
 end
 
-
-until game_end
-
-  $correct_g.each do |x|
+def print_score correct_guesses
+  correct_guesses.each do |x|
     print x
     print " "
   end
+end
+
+def print_end_msg(letters, win)
+
+  print "\nThe game has ended.\n"
+  print win ? "You win!" : "You lose!"
+  print "\n\nThe word was " + letters.join + ".\n\n"
+
+end
+
+def print_screen(guesses_allowed, hints, correct_guesses)
+
+  system ("clear")
+
+  puts "H A N G M A N"
   puts
-  puts "You have #{guesses_allowed} guesses left (#{$hints} hints left)."
+
+  print_score correct_guesses
+
+  puts ""
+  puts ""
+  puts "#{guesses_allowed} guesses left"
+  puts "#{hints} hints left"
+  puts
   print "What is your guess? (Enter '?' for a hint) "
 
-  guess = gets.chomp.to_s.downcase
+end
 
-  if guess == "?"
-    guess = get_hint
-  end
+# Preliminary Stuff
 
-  if guess == nil
-    puts "You have no more hints!"
-    next
-  end
+min_word_length = 5
+max_word_length = 10
 
-  if $guess_list.include? guess
-    puts "You already guessed that."
-    puts "Try again."
-    next
-  end
+print "Loading words ...\n"
+valid_words = load_words("/usr/share/dict/words", min_word_length, max_word_length)
+sorted_letters = get_letter_freq_ascending(valid_words)
 
-  if guess.downcase == word.downcase
-    puts "What an amazing guess!"
-    game_end = true
-    win = true
-  end
+# Main program
 
+game_on = true
 
-  unless $allowed_chars.include? guess
-    puts "That is not a valid character."
-    next
-  else
-    $guess_list.push guess
-  end
+while game_on
 
+  guesses_allowed = 6
+  hints = 3
 
-  if $letters.include? guess
-    0.upto word_length - 1 do |x|
-      if guess == $letters[x]
-        $correct_g[x] = guess
+  ## Select a word
+
+  word = valid_words.sample.downcase.split("")
+  correct_guesses = Array.new(word.count, "_")
+  guess_list = []
+  game_end = false
+  win = false
+
+  ## Start game
+
+  until game_end
+
+    print_screen(guesses_allowed, hints, correct_guesses)
+
+    guess = gets.chomp.to_s.downcase
+
+    if guess.downcase == word.join.downcase
+      puts "What an amazing guess!"
+      game_end = true
+      win = true
+      next
+
+    elsif guess == "?"
+      if hints > 0
+        guess = get_hint(sorted_letters, correct_guesses, word, guess_list)
+        hints -= 1
+      else
+        puts "You have no more hints!"
+        sleep 1
+        next
       end
+
+    elsif guess_list.include? guess
+      puts "You already guessed that."
+      puts "Try again."
+      sleep 1
+      next
+
+    elsif not allowed_chars.include? guess
+      puts "That is not a valid character."
+      sleep 1
+      next
+
+    else
+      guess_list.push guess
+
     end
+
+    if word.include? guess
+      0.upto word.count - 1 do |x|
+        if guess == word[x]
+          correct_guesses[x] = guess
+        end
+      end
+    else
+      guesses_allowed -= 1
+    end
+
+    if correct_guesses == word
+      game_end = true
+      win = true
+    end
+
+    if guesses_allowed == 0
+      game_end = true
+    end
+  end
+
+  print_end_msg(word, win)
+
+  if play_again?
+    next
   else
-    guesses_allowed -= 1
+    game_on = false
   end
 
-  if $correct_g == $letters
-    game_end = true
-    win = true
-  end
-
-  if guesses_allowed == 0
-    game_end = true
-  end
 end
-
-puts "The game has ended."
-
-if win
-  puts "You win!"
-else
-  puts "You lose!"
-end
-
-print "The word was "
-print $letters.join
-puts "."
